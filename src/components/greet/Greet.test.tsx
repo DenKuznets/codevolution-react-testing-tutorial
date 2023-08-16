3b6ee720f1b5f225607cbee853abeeae77c94ef9
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-08-16 13:58:06 +0300

    1

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,17 +1,19 @@
 // Greet should render the text hello and if a name is passed into the component it should render hello followed by the name
 
-import { render, screen } from "@testing-library/react"
-import { Greet } from "./Greet"
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet";
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const element = screen.getByText(/hello/i);
-    expect(element).toBeInTheDocument();
-})
+describe("Greet", () => {
+    test("renders correctly", () => {
+        render(<Greet />);
+        const element = screen.getByText(/hello/i);
+        expect(element).toBeInTheDocument();
+    });
 
-test('Greet renders with a name', () => {
-    const name = 'Denis';
-    render(<Greet name={name} />)
-    const element = screen.getByText(`Hello ${name}`);
-    expect(element).toBeInTheDocument();
-})
+    test("renders with a name", () => {
+        const name = "Denis";
+        render(<Greet name={name} />);
+        const element = screen.getByText(`Hello ${name}`);
+        expect(element).toBeInTheDocument();
+    });
+});
